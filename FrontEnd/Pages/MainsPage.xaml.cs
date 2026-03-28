b0950831e423f8fcf6fec38e19540093cc3cdff6
Arlothea <Adam.s.wingell@gmail.co.uk>
2026-03-28 09:36:55 +0000

    Added api connections to pages

--- FrontEnd/Pages/MainsPage.xaml.cs
+++ FrontEnd/Pages/MainsPage.xaml.cs
@@ -1,14 +1,26 @@
-using System.Collections.ObjectModel;
-using CampusCuisine.Models;
+using CampusCuisine.Services;
 using CampusCuisine.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace CampusCuisine.Pages;
+namespace CampusCuisine;
 
 public partial class MainsPage : ContentPage
 {
   public MainsPage()
   {
     InitializeComponent();
-    BindingContext = new MenuItemViewModel();
+
+    var api = Application.Current!.Services.GetRequiredService<IApiService>();
+    BindingContext = new MenuItemViewModel(api, "Mains");
+  }
+
+  protected override async void OnAppearing()
+  {
+    base.OnAppearing();
+
+    if (BindingContext is MenuItemViewModel vm)
+    {
+      await vm.InitializeAsync();
+    }
   }
 }
